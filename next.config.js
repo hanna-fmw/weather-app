cdcbd9d2dfade1f166749ee05aba87aa05a30d88
Hanna Hosk <hanna.hosk@gmail.com>
2024-08-27 18:42:27 +0200

    Configured Next.js to allow images from cdn.weatherapi.com

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cdn.weatherapi.com',
+				port: '',
+				pathname: '/weather/**',
+			},
+		],
+	},
+}
 
 module.exports = nextConfig
